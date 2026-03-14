57c968fd6ccb10f2bec3a13d1ae5c18f6e59ddf4
gabrielguimaraes2009 <gg5172026@gmail.com>
2026-03-13 21:01:09 -0300

    Primeiro commit.

---
+++ Parte01-Logica_de_programacao/02_variaveis/src/com/variaveis/app/App.java
@@ -0,0 +1,8 @@
+package com.variaveis.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nome;
+        nome = "Gabriel";
+        System.out.println(nome);
+    }
+}
---
+++ Parte01-Logica_de_programacao/03_tipos_variaveis/src/com/tipos/app/App.java
@@ -0,0 +1,18 @@
+package com.tipos.app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // declaração de variáveis
+       String nome = "Gabriel";
+       int idade = 16;
+       double altura = 1.80;
+
+        //    saída de dados
+         System.out.println("Nome: " + nome + ".");
+         System.out.println("Idade: " + idade + ".");
+         System.out.println("Altura: " + altura + "metros.");
+    }
+    
+    
+    
+}
