89f51d3997f8350d302306f1a69f29f248d44892
gabrielguimaraes2009 <gg5172026@gmail.com>
2026-03-20 21:14:41 -0300

    Atividade02

--- Atividades/atividade_02/src/com/atividade02/app/App.java
+++ Atividades/atividade_02/src/com/atividade02/app/App.java
@@ -20,30 +20,68 @@ public class App {
 
         // declaração de variaveis
         String nome;
+        String sala;
+        String filme = null;
         int idade;
-        int sala;
-
+        int idadeMin = 0;
+        
         // instanciar a classe Scanner
         Scanner sc = new Scanner(System.in);
 
-        // laço de repetição
-        do {
+        
+        // entrada de dados
         System.out.println("Informe seu nome:");
         nome = sc.nextLine();
         System.out.println("Informe sua idade:");
         idade = sc.nextInt();
-        System.out.println("Informe o filme desejado");
+
+        sc.nextLine();
+
+        do{
+        System.out.println("Informe o número do filme desejado");
         System.out.println("Sala 01 - A roda Quadrada - Livre");
         System.out.println("Sala 02 - A volta dos Que Não Foram - 12 anos");
         System.out.println("Sala 03 - Poeira em Alto Mar - 14 anos");
         System.out.println("Sala 04 - As tranças do Rei Careca - 16 anos");
         System.out.println("Sala 05 - A vingança do Peixe Frito - 18 anos");
-        sala = sc.nextInt(); 
+        sala = sc.nextLine();
+
+        switch (sala) {
+            case "1":
+                filme = "A roda Quadrada";
+                break;
+            case "2":
+                filme = "A volta dos que não foram";
+                idadeMin = 12;
+                break;
+            case "3":
+                filme = "Poeira em alto mar";
+                idadeMin = 14;
+                break;
+            case "4":
+                filme = "As tranças do Rei Careca";
+                idadeMin = 16;
+            case "5":
+                filme = "A Vingança do Peixe Frito";
+                idadeMin = 18;
+                break;
+            default:
+                idadeMin = idade + 1;    
+
         }
 
-       
- 
-        sc.close();
+        if (idade >= idadeMin) {
+            System.out.println("Ingresso comprado por " + nome + " para");
+        System.out.println("Filme: " + filme + ".");
+        System.out.println("Tenha um bom filme!!! =D");
+        }
+        else {
+            System.out.println("Entrada não liberada para " + nome + ".");
+            System.out.println("Favor escolher outro filme.");
+        }
+    } while(idade <=idadeMin);
 
-    }
+    sc.close();
+
+}
 }
