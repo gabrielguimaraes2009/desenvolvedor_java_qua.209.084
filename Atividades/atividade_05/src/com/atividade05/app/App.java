7e905e4b373ee2e2ef24cc5cf7357dce15dec396
gabrielguimaraes2009 <gg5172026@gmail.com>
2026-03-27 20:47:51 -0300

    Correção atividade 05

--- Atividades/atividade_05/src/com/atividade05/app/App.java
+++ Atividades/atividade_05/src/com/atividade05/app/App.java
@@ -14,9 +14,11 @@ public class App {
         Aluno aluno = new Aluno();
         Professor professor = new Professor();
 
-        double x;
-        double y;
-        double h;
+        double x = 0;
+        double y = 0;
+        double h = 0;
+        double r = 0;
+        String opcao;
 
         // entrada de dados
         System.out.println("Informe seu nome:");
@@ -27,13 +29,51 @@ public class App {
         System.out.println("Informe seu email:");
         aluno.email = sc.nextLine();
 
+        System.out.println("Nome do aluno: " + aluno.nome);
+        System.out.println("Idade do aluno: " + aluno.idade + " anos");
+        System.out.println("E-mail do aluno: " + aluno.email);
+
         professor.nome = "Mário";
         professor.materia = "Matemática";
         professor.matricula = "1234";
         
         System.out.println(professor.apresentar());
 
+        System.out.println("1 - Calcular a area do triângulo");
+        System.out.println("2 - Calcular a area do circulo");
+        System.out.println("3 - Calcular a equação do 1º grau");
+
+        opcao = sc.nextLine();
+
+        switch (opcao) {
         
- 
+            case "1":
+                System.out.println("Informe o valor da base: ");
+                x = sc.nextDouble();
+                System.out.println("Informe o valor da altura:");
+                y = sc.nextDouble();
+                System.out.println("Area do triângulo: " + professor.areaTriangulo(x, h));
+                break;
+            
+            case "2":
+                System.out.println("Informe o valor do raio: ");
+                r = sc.nextDouble();
+                System.out.println("Informe o valor de b:");
+                h = sc.nextDouble ();
+                System.out.println("A raiz do circulo é " + professor.areaCirculo(r));
+            case "3":
+                System.out.println("Informe o valor de a: ");
+                y = sc.nextDouble();
+                r = sc.nextDouble();
+                System.out.println((y != 0) ? "A raiz da equação do 1° grau é " + professor.equacao1Grau(y, r) : "Coeficiente y não pode ser 0.");
+                break;
+                default: 
+                System.out.println("Operação inválida.");
+        }
+
+
+
+
+        sc.close();
     }
 }
--- Atividades/atividade_05/src/com/atividade05/models/Professor.java
+++ Atividades/atividade_05/src/com/atividade05/models/Professor.java
@@ -8,7 +8,18 @@ public class Professor {
     public String apresentar(){
         return "Eu sou " + this.nome + " dou aula de " + this.materia + ", minha matrícula é " + this.matricula + ", informe sua duvida para mim solucioná-la";
     }
-    return 
+
+    public double areaTriangulo(double b, double h) {
+        return (b*h)/2;
+    }
+
+    public double areaCirculo(double r) {
+        return Math.PI*Math.pow(r,2);
+    }
+
+    public double equacao1Grau(double a, double b) {
+        return -b/a;
+    }
 
 
 }
