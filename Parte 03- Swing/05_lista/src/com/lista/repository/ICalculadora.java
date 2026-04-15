608a27280f9eb8deabb1bfdb39f4104d853b64c2
user.pedro1109-dev <user.pedro.s.aguiar@aluno.senai.br>
2026-04-14 21:30:22 -0300

    Lista

---
+++ Parte 03- Swing/05_lista/src/com/lista/repository/ICalculadora.java
@@ -0,0 +1,9 @@
+package com.lista.repository;
+
+public interface ICalculadora {
+    public double somar();
+    public double subtrair();
+    public double multiplicar();
+    public double dividir ();
+
+}
